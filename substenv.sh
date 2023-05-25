22c25dfcd8cedb3d3547d31209d0595b84349400
euloge.tie@pytechnologies.net <euloge.tie@pytechnologies.net>
2023-05-25 00:13:18 +0000

    Initial commit

---
+++ substenv.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+####
+# This script scans the input file to discover variables, 
+# then Prompt the user for values for the discovered variables which are
+# and finally Generate the output file by substituting variables
+# in the input file using envsubst utility
+#
+#
+#####
+
+
+# ____ Scan the input file to discover variables ____
+# Scans the input file and extracts variables
+# by searching for patterns like $variable or ${variable}. 
+# The discovered variables ar stored in an array.
+scan_input_file() {
+  local input="$1"
+  local variables=()
+
+  while IFS= read -r line; do
+    
+    # Skip comment lines starting with #
+    if [[ $line =~ ^[[:space:]]*# ]]; then
+      continue
+    fi
+    
+    # Match $variable or ${variable} patterns
+    while [[ $line =~ \$\{?([A-Za-z_][A-Za-z0-9_]*)\}? ]]; do
+      match="${BASH_REMATCH[0]}"
+      var="${BASH_REMATCH[1]}"
+      line=${line/"$match"/""}
+
+      variables+=("$var")
+    done
+  done <  <(printf '%s\n' "$input")
+
+  # Remove duplicates and store unique variables
+  variables=($(echo "${variables[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+  # One liner for variable discovery using envsubst with the -v|--variable switch
+  #variables=($(envsubst $input -v | sort -u | tr '\n' ' '))
+
+  echo "${variables[@]}"
+}
+
+## ____ Prompt the user for values for the discovered variables ____
+# Takes an array of discovered variables and prompts the user to enter values for each variable. 
+# Each entered value is assigned to the corresponding variable
+prompt_user_for_values() {
+  local variables=("$@")
+
+  # Iterate over the discovered variables array and pick each variable name into the $var loop variable
+  for var in "${variables[@]}"; do
+
+    # Prompt for a value for the current variable picked into $var
+    read -p "Enter the value for $var [${!var}]: " value
+
+    # Assign the user input to the variable picked into $var.
+    # Overwrite the existing value only if user input is not empty
+    eval "$var=\"${value:-\$$var}\""
+
+  done
+}
+
+##### Main script logic
+
+# Check if command-line arguments are provided
+if [[ $# -lt 2 ]]; then
+  echo "Error: Missing argument(s)! any of input and output file name is not provided." >&2
+  exit 1
+fi
+
+# Store standard input data into $input
+input=$(cat)
+
+# Scanning input file for variables
+discovered_variables=($(scan_input "$input"))
+
+# Prompting the user for values for the discovered variables
+prompt_user_for_values "${discovered_variables[@]}"
+
+# Generate the output from $input by performing variable substitution using envsubst 
+echo $input | envsubst 
