d3b02c9ee1ab1674bf30cebff04e23075d47964c
euloge.tie@pytechnologies.net <euloge.tie@pytechnologies.net>
2023-05-26 02:29:35 +0000

    No command line option supported

--- substenv.sh
+++ substenv.sh
@@ -1,64 +1,65 @@
 #!/usr/bin/env bash
 
 ####
-# This script scans the input file to discover variables, 
-# then Prompt the user for values for the discovered variables which are
-# and finally Generate the output file by substituting variables
-# in the input file using envsubst utility
-#
-#
+# This is a wrapper script for envsubst utility which adds extra functionnalities like:
+#        - Interactive variable discorvery and assignment
+# Todo   - Variable exclusion and escaping mechanisms(ingore unset variables, ignore explicit list of variables, ...)
+#        - etc.
 #####
 
+## 
+# Discovers variables contained in the argument and 
+# outputs an array containing the discovered variable names
+###
+scan_input() {
 
-# ____ Scan the input file to discover variables ____
-# Scans the input file and extracts variables
-# by searching for patterns like $variable or ${variable}. 
-# The discovered variables ar stored in an array.
-scan_input_file() {
   local input="$1"
   local variables=()
 
-  # One liner for variable discovery using envsubst with the -v|--variable switch
-  variables=($(envsubst $input -v | sort -u | tr '\n' ' '))
+  # One liner for variable discovery using envsubst with the -v|--variables switch
+  variables=("$(envsubst -v "$input"  | sort -u | tr '\n' ' ')")
 
+  # Outputs the array of variable names
   echo "${variables[@]}"
 }
 
-## ____ Prompt the user for values for the discovered variables ____
-# Takes an array of discovered variables and prompts the user to enter values for each variable. 
-# Each entered value is assigned to the corresponding variable
-prompt_user_for_values() {
+## 
+# Sets variables respectively named after the arguments 
+# and assigned from user inputs
+###
+prompt_for_values() {
+
+  # Stores arguments in a local array variable
   local variables=("$@")
 
-  # Iterate over the discovered variables array and pick each variable name into the $var loop variable
+  # Iterates over the array variable, assigning a loop variable ($var) with each element successively
   for var in "${variables[@]}"; do
 
-    # Prompt for a value for the current variable picked into $var
-    read -p "Enter the value for $var [${!var}]: " value
+    # Prompts for a value aimed at assigning a variable named after the current value of $var.
+    # Indirect expansion (like ${!var}) is used to display the current value of the variable if it's already set
+    # </dev/tty enforces reading from the terminal instead of Stdin. Otherwize the user won't be prompted
+    read -p "Enter the value for $var [${!var}]: " value < /dev/tty
 
-    # Assign the user input to the variable picked into $var.
-    # Overwrite the existing value only if user input is not empty
-    eval "$var=\"${value:-\$$var}\""
+    # Sets the variable named from the current value of $var and assigned with the user input
+    # Existing variable value is only overwritten with non-empty user input
+    eval "export $var=\"${value:-\$$var}\""
 
   done
 }
 
-##### Main script logic
+##### MAIN SCRIPT LOGIC
 
-# Check if command-line arguments are provided
-if [[ $# -lt 2 ]]; then
-  echo "Error: Missing argument(s)! any of input and output file name is not provided." >&2
-  exit 1
-fi
+#TODO: Enhance by handling commmand line args, at least those supported by envsubst
 
-# Store standard input data into $input
+# Capture standard input content
 input=$(cat)
 
-# Scanning input file for variables
+# Dicover variables from the standard input content
 discovered_variables=($(scan_input "$input"))
 
-# Prompting the user for values for the discovered variables
-prompt_user_for_values "${discovered_variables[@]}"
+# Prompt the user for values for the discovered variables
+prompt_for_values "${discovered_variables[@]}"
 
-# Generate the output from $input by performing variable substitution using envsubst 
-echo $input | envsubst 
+# Generate the output from the input content by performing variable substitution using envsubst utility
+#envsubst < <(echo "$input") # Process substitution flavor
+envsubst <<< "$input" # Here-String flavor
